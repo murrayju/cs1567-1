e447e92d6f603764ae4406147fc6684d086d116d
Justin <murrayju@gmail.com>
2009-01-28 21:35:30 -0500

    The robot can move!

--- PIDlib/PIDlib.h
+++ PIDlib/PIDlib.h
@@ -3,7 +3,9 @@
 
 #include <libplayerc/playerc.h>
 
+#define PI 3.141592654
+
 
 int bumped(playerc_bumper_t *);
 
-double Move(playerc_client_t * client, playerc_posiiton2d_t * pos2D, playerc_bumper_t * bumper, double meters);
+double Move(playerc_client_t * client, playerc_position2d_t * pos2D, playerc_bumper_t * bumper, double X, double Y);
